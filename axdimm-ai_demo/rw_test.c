a39f413a33b78c6329cfe68e4d18e238317935b1
Neel Patel <n869p538@ittc.ku.edu>
2022-04-19 19:56:03 -0500

    single bit start

--- axdimm-ai_demo/rw_test.c
+++ axdimm-ai_demo/rw_test.c
@@ -98,22 +98,8 @@ void ax_single(void)
 
 	ulong i;
 
-	/*
-	for (i = 0; i < 0x200; i += 1)
-	{
-		mb();memcpy((void *)(addr + i * 256 + 0x000000000), (void *)EmbTable[2 * i%0x200], 64);
-		mb();memcpy((void *)(addr + i * 256 + 0x400000000), (void *)EmbTable[2 * i%0x200], 64);
-		mb();memcpy((void *)(addr + i * 256 + 0x000000040), (void *)EmbTable[2 * i%0x200 + 1], 64);
-		mb();memcpy((void *)(addr + i * 256 + 0x400000040), (void *)EmbTable[2 * i%0x200 + 1], 64);
-	}
-	*/
-	//mb();memcpy((void *)(addr + 256 + 0x000000000), (void *)Buffer,64);
-	/*
-	mb();memcpy((void *)(addr + 256 + 0x000000000), (void *)data,16);
-	mb();memcpy((void *)(addr + 256 + 0x000000000), (void *)data,16);
-	*/
+
 	mb();memcpy((void *)(addr + 256 + 0x000000000), (void *)(&t_2),1);
-	//mb();memcpy((void *)(addr + 256 + 0x000000000), (void *)data,16);
 
 	printk("BufferCopy Done=============================\n");
 
@@ -127,24 +113,6 @@ void ax_single(void)
 
 	//getnstimeofday(&start);
 
-	//what is this?
-	if (num_of_inst_per_rank <= 8)
-	{
-		memcpy((void *)(addr + 0x7C0000000), (void *)NMPInst[0], 64);
-		memcpy((void *)(addr + 0xFC0000000), (void *)NMPInst[0], 64);
-	}
-	else
-	{
-		for (i = 0; i < (num_of_inst_per_rank / 16); i += 1)
-		{
-			memcpy((void *)(addr + i * 256 + 0x3E0000000), (void *)NMPInst[2 * i], 64);
-			memcpy((void *)(addr + i * 256 + 0x3E0000040), (void *)NMPInst[2 * i + 1], 64);
-			memcpy((void *)(addr + i * 256 + 0x7E0000000), (void *)NMPInst[2 * i], 64);
-			memcpy((void *)(addr + i * 256 + 0x7E0000040), (void *)NMPInst[2 * i + 1], 64);
-		}
-	}
-
-	//getnstimeofday(&endinst);
 
 	//are these addresses of registers?
 	printk("NMPInst copies Done=============================\n");
@@ -153,57 +121,7 @@ void ax_single(void)
 	
     udelay(1);
 	breakcount = 0;
-	/*
-	do
-	{
-		breakcount++;
-		mb();memcpy((void *)polldata0, (void *)(addr + 0x3E1000440), sizeof(polldata0));
-		printk("Rank0.PC             (0x%08x)\n", polldata0[0]);
-		printk("Rank0.PSUM_POOL_DONE (0x%08x)\n", polldata0[1]);
-		if (breakcount > 100)
-		{
-			printk("Rank0.Polling Failed");
-			break;
-		}
-	} while (polldata0[1] != 0xffffffff);
-	*/
 
-	/*
-	breakcount = 0;
-	do
-	{
-		breakcount++;
-		mb();memcpy((void *)polldata1, (void *)(addr + 0x7E1000440), sizeof(polldata1));
-		printk("Rank1.PC             (0x%08x)\n", polldata1[0]);
-		printk("Rank1.PSUM_POOL_DONE (0x%08x)\n", polldata1[1]);
-		if (breakcount > 100)
-		{
-			printk("Rank1.Polling Failed");
-			break;
-		}
-	} while (polldata1[1] != 0xffffffff);
-	*/
-
-	//getnstimeofday(&endsls);
-
-	/*
-	if (breakcount <= 100)
-	{
-		memcpy((void *)readdata0, (void *)(addr + 0x3E4020000), sizeof(readdata0));
-		memcpy((void *)readdata1, (void *)(addr + 0x3E4020040), sizeof(readdata1));
-		memcpy((void *)readdata2, (void *)(addr + 0x7E4020000), sizeof(readdata2));
-		memcpy((void *)readdata3, (void *)(addr + 0x7E4020040), sizeof(readdata3));
-
-		//getnstimeofday(&end);
-		
-		printk("Rank0.PSUM= 0x%08x\n", readdata0[0]);
-		printk("Rank0.PSUM= 0x%08x\n", readdata1[0]);
-
-		printk("Rank1.PSUM= 0x%08x\n", readdata2[0]);
-		printk("Rank1.PSUM= 0x%08x\n", readdata3[0]);
-
-	}
-	*/
 
 	//SLS_EXEC UNSET
 	reg_data[0] = 0x0;
@@ -214,198 +132,22 @@ void ax_single(void)
 	reg_data[0] = 0x0;
 	memcpy((void *)(addr + 0x3E1000000), (void *)reg_data, 64);
 	memcpy((void *)(addr + 0x7E1000000), (void *)reg_data, 64);
+
+	printk("At copied loc: 0x%.8x\n", *(uint*)(addr + 256 + 0x000000000) );
 }
 
-void ax_perf_test(void)
+void ax_read(void)
 {
-	int num_of_inst_per_rank;
-	int breakcount;
-	ulong i;
-
-	for (i = 0; i < 0x200; i += 1)
-	{
-		mb();memcpy((void *)(addr + i * 256 + 0x000000000), (void *)EmbTable[2 * i%0x200], 64);
-		mb();memcpy((void *)(addr + i * 256 + 0x800000000), (void *)EmbTable[2 * i%0x200], 64);
-		mb();memcpy((void *)(addr + i * 256 + 0x000000040), (void *)EmbTable[2 * i%0x200 + 1], 64);
-		mb();memcpy((void *)(addr + i * 256 + 0x800000040), (void *)EmbTable[2 * i%0x200 + 1], 64);
-		mb();memcpy((void *)(addr + i * 256 + 0x000000080), (void *)EmbTable[2 * i%0x200], 64);
-		mb();memcpy((void *)(addr + i * 256 + 0x800000080), (void *)EmbTable[2 * i%0x200], 64);
-		mb();memcpy((void *)(addr + i * 256 + 0x0000000c0), (void *)EmbTable[2 * i%0x200 + 1], 64);
-		mb();memcpy((void *)(addr + i * 256 + 0x8000000c0), (void *)EmbTable[2 * i%0x200 + 1], 64);
-	}
-
-	printk("wr_EmbTable Done=============================\n");
-
-	//CONF_SYSTEM_EN SET
-	reg_data[0] = 0xcafe;
-	memcpy((void *)(addr + 0x7C2000000), (void *)reg_data, 64);
-	memcpy((void *)(addr + 0xFC2000000), (void *)reg_data, 64);
-	memcpy((void *)(addr + 0x7C2000080), (void *)reg_data, 64);
-	memcpy((void *)(addr + 0xFC2000080), (void *)reg_data, 64);
-
-	//WR_INST
-	num_of_inst_per_rank = sizeof(NMPInst) / 8;
-
-	//getnstimeofday(&start);
-
-	if (num_of_inst_per_rank <= 8)
-	{
-		memcpy((void *)(addr + 0x7C0000000), (void *)NMPInst[0], 64);
-		memcpy((void *)(addr + 0xFC0000000), (void *)NMPInst[0], 64);
-	}
-	else
-	{
-		for (i = 0; i < (num_of_inst_per_rank / 16); i += 1)
-		{
-			memcpy((void *)(addr + i * 256 + 0x7C0000000), (void *)NMPInst[2 * i], 64);
-			memcpy((void *)(addr + i * 256 + 0x7C0000040), (void *)NMPInst[2 * i + 1], 64);
-			memcpy((void *)(addr + i * 256 + 0xFC0000000), (void *)NMPInst[2 * i], 64);
-			memcpy((void *)(addr + i * 256 + 0xFC0000040), (void *)NMPInst[2 * i + 1], 64);
-			memcpy((void *)(addr + i * 256 + 0x7C0000080), (void *)NMPInst[2 * i], 64);
-			memcpy((void *)(addr + i * 256 + 0x7C00000c0), (void *)NMPInst[2 * i + 1], 64);
-			memcpy((void *)(addr + i * 256 + 0xFC0000080), (void *)NMPInst[2 * i], 64);
-			memcpy((void *)(addr + i * 256 + 0xFC00000c0), (void *)NMPInst[2 * i + 1], 64);
-		}
-	}
-
-	//getnstimeofday(&endinst);
-
-	printk("wr_InstBuf Done=============================\n");
-	memcpy((void *)(addr + 0x7C2000040), (void *)reg_data, 64);
-	memcpy((void *)(addr + 0xFC2000040), (void *)reg_data, 64);
-	memcpy((void *)(addr + 0x7C20000c0), (void *)reg_data, 64);
-	memcpy((void *)(addr + 0xFC20000c0), (void *)reg_data, 64);
-
-    udelay(1);
-	breakcount = 0;
-	do
-	{
-		breakcount++;
-		memcpy((void *)polldata0, (void *)(addr + 0x7C2000840), sizeof(polldata0));
-		printk("Rank0.PC             (0x%08x)\n", polldata0[0]);
-		printk("Rank0.PSUM_POOL_DONE (0x%08x)\n", polldata0[1]);
-		if (breakcount > 100)
-		{
-			printk("Rank0.Polling Failed");
-			break;
-		}
-	} while (polldata0[1] != 0xffffffff);
-
-	breakcount = 0;
-	do
-	{
-		breakcount++;
-		memcpy((void *)polldata1, (void *)(addr + 0xFC2000840), sizeof(polldata1));
-		printk("Rank1.PC             (0x%08x)\n", polldata1[0]);
-		printk("Rank1.PSUM_POOL_DONE (0x%08x)\n", polldata1[1]);
-		if (breakcount > 100)
-		{
-			printk("Rank1.Polling Failed");
-			break;
-		}
-	} while (polldata1[1] != 0xffffffff);
-
-	breakcount = 0;
-	do
-	{
-		breakcount++;
-		memcpy((void *)polldata0, (void *)(addr + 0x7C20008c0), sizeof(polldata0));
-		printk("Rank2.PC             (0x%08x)\n", polldata0[0]);
-		printk("Rank2.PSUM_POOL_DONE (0x%08x)\n", polldata0[1]);
-		if (breakcount > 100)
-		{
-			printk("Rank0.Polling Failed");
-			break;
-		}
-	} while (polldata0[1] != 0xffffffff);
-
-	breakcount = 0;
-	do
-	{
-		breakcount++;
-		memcpy((void *)polldata1, (void *)(addr + 0xFC20008c0), sizeof(polldata1));
-		printk("Rank3.PC             (0x%08x)\n", polldata1[0]);
-		printk("Rank3.PSUM_POOL_DONE (0x%08x)\n", polldata1[1]);
-		if (breakcount > 100)
-		{
-			printk("Rank1.Polling Failed");
-			break;
-		}
-	} while (polldata1[1] != 0xffffffff);
-
-	//getnstimeofday(&endsls);
-
-	if (breakcount <= 100)
-	{
-		
-
-		memcpy((void *)readdata0, (void *)(addr + 0x7C8040000), sizeof(readdata0));
-		memcpy((void *)readdata1, (void *)(addr + 0x7C8040040), sizeof(readdata1));
-		memcpy((void *)readdata2, (void *)(addr + 0xFC8040000), sizeof(readdata2));
-		memcpy((void *)readdata3, (void *)(addr + 0xFC8040040), sizeof(readdata3));
-
-		memcpy((void *)readdata4, (void *)(addr + 0x7C8040080), sizeof(readdata4));
-		memcpy((void *)readdata5, (void *)(addr + 0x7C80400c0), sizeof(readdata5));
-		memcpy((void *)readdata6, (void *)(addr + 0xFC8040080), sizeof(readdata6));
-		memcpy((void *)readdata7, (void *)(addr + 0xFC80400c0), sizeof(readdata7));
-
-		//getnstimeofday(&end);
-		
-		printk("Rank0.PSUM= 0x%08x\n", readdata0[0]);
-		printk("Rank0.PSUM= 0x%08x\n", readdata1[0]);
-
-		printk("Rank1.PSUM= 0x%08x\n", readdata2[0]);
-		printk("Rank1.PSUM= 0x%08x\n", readdata3[0]);
-
-		printk("Rank2.PSUM= 0x%08x\n", readdata4[0]);
-		printk("Rank2.PSUM= 0x%08x\n", readdata5[0]);
-
-		printk("Rank3.PSUM= 0x%08x\n", readdata6[0]);
-		printk("Rank3.PSUM= 0x%08x\n", readdata7[0]);
-	}
-
-	//SLS_EXEC UNSET
-	reg_data[0] = 0x0;
-	memcpy((void *)(addr + 0x7C2000040), (void *)reg_data, 64);
-	memcpy((void *)(addr + 0xFC2000040), (void *)reg_data, 64);
-	memcpy((void *)(addr + 0x7C20000c0), (void *)reg_data, 64);
-	memcpy((void *)(addr + 0xFC20000c0), (void *)reg_data, 64);
-
-	//CONF_SYSTEM_EN_UNSET
-	reg_data[0] = 0x0;
-	memcpy((void *)(addr + 0x7C2000000), (void *)reg_data, 64);
-	memcpy((void *)(addr + 0xFC2000000), (void *)reg_data, 64);
-	memcpy((void *)(addr + 0x7C2000080), (void *)reg_data, 64);
-	memcpy((void *)(addr + 0xFC2000080), (void *)reg_data, 64);
+	printk("READ");
 }
 
 static int mem_init(void)
 {
-	uint i=0;
-	uint j=0;
 	printk("MEM INIT");
-
-	for (i=0; i<1024; i++)
-		for (j=0; j<16; j++)
-			Buffer[i][j] = 0xcccccccc;
-	/*
-
-	for (i=0; i<128; i++)
-	{
-		for (j=0; j<4; j++)
-		{
-			NMPInst[i][4*j] = 0x0;
-			NMPInst[i][4*j+1] = 0x0;
-			NMPInst[i][4*j+2] = 0x0;
-			NMPInst[i][4*j+3] = 0x20000;
-		}
-	}
-	NMPInst[127][3]=0x30000;
-	*/
-
 	switch (test)
 	{
 	case 0:
+	case 1:
 		addr = memremap(0x100000000, 0x800000000, MEMREMAP_WC);
 		break;
 	default:
@@ -418,20 +160,19 @@ static int mem_init(void)
 
 static void mem_exit(void)
 {
-	t_2=(char)255;
+	t_2=(char)1;
 	switch (test)
 	{
 	case 0:
 		ax_single();
 		break;
 	case 1:
-		ax_perf_test();
+		ax_read();
 		break;
 	default:
 		printk("\n\n\nWrong Test Case\n\n\n");
 		break;
 	}
-	printk("At copied loc: 0x%.8x\n", *(uint*)(addr + 256 + 0x000000000) );
 	memunmap((void *)addr);
 	printk("Module exit\n");
 }
