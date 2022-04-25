add9a3570557357fb146bcde30b83a1b8b4f6347
Neel Patel <n869p538@ittc.ku.edu>
2022-04-25 12:29:21 -0500

    test char copy

--- axdimm-ai_demo/rw_test.c
+++ axdimm-ai_demo/rw_test.c
@@ -25,18 +25,32 @@ module_param(axdimm_addr,  ulong , 0644);
 static uint test = 1;
 module_param(test, uint, 0644);
 
-static uint data = 1;
-module_param(data, uint, 0644);
-
 volatile void *addr;
 
 atomic_t t = ATOMIC_INIT(0);
 
+int copy_char(void)
+{
+	char c = 'c';
+	printk("copying char %c", c);
+	memcpy( (void *) (addr + 256) , (void *) &c, sizeof(c));
+	printk("char at loc(%lu):%c", (unsigned long)addr, c);
+	return 0;
+}
 static int mem_init(void)
 {
 	printk("MEM INIT");
 	addr = memremap(0x100000000, 0x800000000, MEMREMAP_WC);
 	axdimm_addr=(ulong )addr;
+	switch (test)
+	{
+		case 0:
+			copy_char();	
+		case 1:
+		default:
+			break;
+	}
+
 	return 0;
 }
 static void mem_exit(void)
