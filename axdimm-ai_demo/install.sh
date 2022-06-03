bdd73f397b533f78c51ad447db5eb4320b125bec
Neel Patel <n869p538@ittc.ku.edu>
2022-06-03 15:27:33 -0500

    better output

--- axdimm-ai_demo/install.sh
+++ axdimm-ai_demo/install.sh
@@ -5,17 +5,20 @@ module=rw_test
 if [ -z "$(sudo lsmod | grep $module)" ]; then
 	sudo dmesg -C
 	sudo dmesg -D
-	make
+	make -s
 	sudo dmesg -E
-	sudo insmod rw_test.ko $*
+	sudo insmod rw_test.ko $* > /dev/null
+	[ "$?" != "0" ] && echo "[Fatal]: module could not be loaded -- check if $module exists"
 	sudo dmesg | tee install.out
+	#sudo cp -f rw_test.ko /lib/modules/`uname -r` #add symlink so modprobe can find
 else 
 	echo "already installed... uninstalling..."
 	sudo dmesg -C
 	sudo dmesg -D
-	make clean
+	make -s clean
 	sudo dmesg -E
-	sudo rmmod rw_test.ko
+	sudo rmmod rw_test.ko &> /dev/null
+	[ "$?" != "0" ] && echo "[Fatal]: module improperly loaded/unloaded -- reboot to uninstall"
 	sudo dmesg | tee uninstall.out
 fi
 
