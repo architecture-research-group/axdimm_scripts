72e7dd95dd6f8230638abc19f8dfd0f0ef275bec
Neel Patel <n869p538@ittc.ku.edu>
2022-06-07 14:59:41 -0500

    module missing check

--- axdimm-ai_demo/install.sh
+++ axdimm-ai_demo/install.sh
@@ -8,7 +8,7 @@ if [ -z "$(sudo lsmod | grep $module)" ]; then
 	make -s
 	sudo dmesg -E
 	sudo insmod rw_test.ko $* > /dev/null
-	[ "$?" != "0" ] && echo "[Fatal]: module could not be loaded -- check if $module exists"
+	[ "$?" != "0" ] && echo "[Fatal]: module could not be loaded -- check if $module exists" && exit -1
 	sudo dmesg | tee install.out
 	#sudo cp -f rw_test.ko /lib/modules/`uname -r` #add symlink so modprobe can find
 else 
