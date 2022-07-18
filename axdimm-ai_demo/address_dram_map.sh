8948b2e2b2c23f412cb62bae1cfa6ef4b91200da
Neel Patel <n869p538@ittc.ku.edu>
2022-07-18 13:12:13 -0500

    map to 32 MB ++ address mapping w/ tcl instructions

--- axdimm-ai_demo/address_dram_map.sh
+++ axdimm-ai_demo/address_dram_map.sh
@@ -4,14 +4,13 @@ source common.sh
 
 [ ! -z "$( lsmod | grep rw_test)" ] && ./uninstall.sh
 
-max=$(( 1024 * 32 ))
+max=$(( 1024 * 1024 * 32 ))
 #max=$(( 64 * 3 ))
 
 ./uninstall.sh
-ctr=1
-for i in `seq 0 64 $max` ; do
+for i in `seq $(( 1024 * 32 )) 64 $max` ; do
 	echo "$i, $( offset_to_addr $i)"
-	./install.sh test=1 offset=$i
+	./install.sh test=1 str=\"hello$(( $i / 64 ))\" offset=$i
 	./uninstall.sh
 	echo "iteration: $ctr complete"
 	ctr=$(( ctr + 1 ))
