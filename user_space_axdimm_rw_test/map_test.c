1a44a8d751a0f2ca60d2dad935b4ecab6acaaaea
Neel Patel <n869p538@ittc.ku.edu>
2022-07-21 22:44:43 -0500

    get opt ++ multi_cache line write flush

--- user_space_axdimm_rw_test/map_test.c
+++ user_space_axdimm_rw_test/map_test.c
@@ -24,81 +24,9 @@
 # include <x86intrin.h>
 
 //ins
-#define B64 (63 << 1) 
-#define B63 (62 << 1) 
-#define B62 (61 << 1) 
-#define B61 (60 << 1) 
-#define B60 (59 << 1) 
-#define B59 (58 << 1) 
-#define B58 (57 << 1) 
-#define B57 (56 << 1) 
-#define B56 (55 << 1) 
-#define B55 (54 << 1) 
-#define B54 (53 << 1) 
-#define B53 (52 << 1) 
-#define B52 (51 << 1) 
-#define B51 (50 << 1) 
-#define B50 (49 << 1) 
-#define B49 (48 << 1) 
-#define B48 (47 << 1) 
-#define B47 (46 << 1) 
-#define B46 (45 << 1) 
-#define B45 (44 << 1) 
-#define B44 (43 << 1) 
-#define B43 (42 << 1) 
-#define B42 (41 << 1) 
-#define B41 (40 << 1) 
-#define B40 (39 << 1) 
-#define B39 (38 << 1) 
-#define B38 (37 << 1) 
-#define B37 (36 << 1) 
-#define B36 (35 << 1) 
-#define B35 (34 << 1) 
-#define B34 (33 << 1) 
-#define B33 (32 << 1) 
-#define B32 (31 << 1) 
-#define B31 (30 << 1) 
-#define B30 (29 << 1) 
-#define B29 (28 << 1) 
-#define B28 (27 << 1) 
-#define B27 (26 << 1) 
-#define B26 (25 << 1) 
-#define B25 (24 << 1) 
-#define B24 (23 << 1) 
-#define B23 (22 << 1) 
-#define B22 (21 << 1) 
-#define B21 (20 << 1) 
-#define B20 (19 << 1) 
-#define B19 (18 << 1) 
-#define B18 (17 << 1) 
-#define B17 (16 << 1) 
-#define B16 (15 << 1) 
-#define B15 (14 << 1) 
-#define B14 (13 << 1) 
-#define B13 (12 << 1) 
-#define B12 (11 << 1) 
-#define B11 (10 << 1) 
-#define B10 (9 << 1) 
-#define B9 (8 << 1) 
-#define B8 (7 << 1) 
-#define B7 (6 << 1) 
-#define B6 (5 << 1) 
-#define B5 (4 << 1) 
-#define B4 (3 << 1) 
-#define B3 (2 << 1) 
-#define B2 (1 << 1) 
-#define B1 (0 << 1) 
-
-#define F1 (B8 | B10 )
-#define F2 (B8 | B11 )
-#define F3 (B12 | B14 )
-#define F4 (B12 | B16 )
-#define F6 (B12 | B17 )
-#define F7 (B18 | B19 )
-
 #define PG_SZ 4096
 
-void read_dev_dram(uint64_t base, uint64_t offset, uint64_t size){
+void read_dev_dram(uint64_t base, uint64_t offset, int size){
 	int rd_fd;
 	char * rd_data = (char *)malloc(sizeof(char) * size);
 	uint64_t pg_off = offset / PG_SZ;
@@ -118,12 +46,12 @@ void read_dev_dram(uint64_t base, uint64_t offset, uint64_t size){
 		exit(-1);
 	}
 
-	printf( "reading memory address: 0x%016lx \n", base + offset);
+	printf( "reading %d bytes from memory address: 0x%016lx \n", size,  base + offset);
 
 
-	_mm_clflush( pg_al_loc + (offset % PG_SZ) );
-	memcpy( (void *) rd_data, (void *) ( pg_al_loc + (offset % PG_SZ) ), 1 );
-	printf( "read: %s\n", rd_data);
+	_mm_clflush( pg_al_loc + (offset%PG_SZ) );
+	memcpy( (void *) rd_data, (void *) ( pg_al_loc + offset) , size );
+	printf( "data: %s\n", rd_data);
 	return;
 }
 
@@ -138,31 +66,70 @@ void write_dev_dram(uint64_t base, uint64_t offset, char * data, int size){
 	}
 
 
-	char * pg_al_loc = (char *) mmap(NULL, PG_SZ, PROT_READ | PROT_WRITE, MAP_FILE | MAP_SHARED, rd_fd, base + (pg_off * PG_SZ) );
+	char * pg_al_loc = (char *) mmap(NULL, PG_SZ*2, PROT_READ | PROT_WRITE, MAP_FILE | MAP_SHARED, rd_fd, base + (pg_off * PG_SZ) );
 
 	if (pg_al_loc == -1)
 	{
 		printf("could not mmap\n");
 		exit(-1);
 	}
-	printf( "writing 'a' to memory address: 0x%016lx \n", base + offset);
+	printf( "writing %s to memory address: 0x%016lx \n", data,  base + offset);
 
-	_mm_clflush( pg_al_loc + (offset % PG_SZ) );
-	memcpy( (void *) ( pg_al_loc + (offset % PG_SZ) ), (void *) data, size );
+	memcpy( (void *) ( pg_al_loc + (offset %PG_SZ) ), (void *) data, size );
+    /* flush cache lines of all data */
+	_mm_clflush( pg_al_loc + (offset%PG_SZ) );
+    for (int i=0; i<size; i+=64){
+        _mm_clflush( pg_al_loc + (offset%PG_SZ) + (i * 64) );
+    }
+    _mm_clflush( pg_al_loc + (offset%PG_SZ) + 64);
+    /* flush extra cache line in case data extends to next cache line */
 }
 int main(int argc, char ** argv)
 {
 	
+
 	uint64_t base=0x100000000;
-	uint64_t tgt=B7; /*write to this memory location*/
-	if (argc == 2)
-	{
-		tgt = atoi(argv[1]);
-		printf( "using argument offset:%016lx\n",tgt );
-	}
+	uint64_t offset=( 9 << 1 ) ; /*write to this memory location*/
+
+	char *wr_data;
+    int do_write=0;
+    int do_read=0;
 	int size=sizeof(char) * 8;
+
+	int opt;
+	while ((opt = getopt(argc, argv, "rwo:s:p:")) != -1) {
+	   switch (opt) {
+	   case 'p':
+            wr_data = (char *)malloc(sizeof(char) * strlen(optarg));
+            wr_data = strncpy(wr_data, optarg, strlen(optarg));
+	   		break;
+	   case 's':
+            size=atoi(optarg);
+            printf("size:%d\n", size);
+	   		break;
+	   case 'r':
+           do_read=1;
+		   break;
+	   case 'w':
+           do_write=1;
+		   break;
+       case 'o':
+           offset=strtoul(optarg, NULL, 10);
+           break;
+	   default: /* '?' */
+		   fprintf(stderr, "Usage: %s [-r] [-w] [-s size] [-p string] [-o offset] \n",
+				   argv[0]);
+		   exit(EXIT_FAILURE);
+	   }
+	}
+    /*
 	char * data = (char *) malloc(size);
 	data[0]='a';
-	write_dev_dram(base, tgt, data, size );
-	read_dev_dram(base, tgt, size );
+    */
+
+    if (do_write)
+        write_dev_dram(base, offset, wr_data, size );
+    if (do_read)
+        read_dev_dram(base, offset, size );
+    
 }
