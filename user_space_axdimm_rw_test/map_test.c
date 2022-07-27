ec5e8111da1bbc6bd5adc71042fd21c1f5bbcb4c
Neel Patel <n869p538@ittc.ku.edu>
2022-07-27 16:57:13 -0500

    fence between cache line options

--- user_space_axdimm_rw_test/map_test.c
+++ user_space_axdimm_rw_test/map_test.c
@@ -27,45 +27,10 @@
 //ins
 #define PG_SZ 4096
 
-void read_write(uint64_t base, uint64_t offset, char * data, int size)
-{
-	int rd_fd;
-	char * rd_data = (char *)malloc(sizeof(char) * size);
-	uint64_t pg_off = offset / PG_SZ;
+int use_fence = 0;
+int use_flush = 0;
+int use_cache_fence = 0;
 
-	if ((rd_fd = open("/dev/mem", O_RDWR)) < 0)
-	{
-		printf("Error: could not open RDIMM character device\n");
-		exit(-1);
-	}
-
-
-	char * pg_al_loc = (char *) mmap(NULL, PG_SZ*2, PROT_READ | PROT_WRITE, MAP_FILE | MAP_SHARED, rd_fd, base + (pg_off * PG_SZ) );
-
-	if (pg_al_loc == -1)
-	{
-		printf("could not mmap\n");
-		exit(-1);
-	}
-
-
-	printf( "writing %s to memory address: 0x%016lx \n", data,  base + offset);
-	memcpy( (void *) ( pg_al_loc + (offset %PG_SZ) ), (void *) data, size );
-
-    /* flush cache lines of all data */
-	_mm_clflush( pg_al_loc + (offset%PG_SZ) );
-    for (int i=0; i<size; i+=64){
-        _mm_clflush( pg_al_loc + (offset%PG_SZ) + (i * 64) );
-    }
-    _mm_clflush( pg_al_loc + (offset%PG_SZ) + 64);
-
-
-	printf( "reading %d bytes from memory address: 0x%016lx \n", size,  base + offset);
-	memcpy( (void *) rd_data, (void *) ( pg_al_loc + (offset % PG_SZ) ) , size );
-	printf( "data: %s\n", rd_data);
-	return;
-
-}
 
 void read_dev_dram(uint64_t base, uint64_t offset, int size){
 	int rd_fd;
@@ -93,12 +58,18 @@ void read_dev_dram(uint64_t base, uint64_t offset, int size){
 	printf( "reading %d bytes from memory address: 0x%016lx \n", size,  base + offset);
 
 
-	_mm_clflush( pg_al_loc + (offset%PG_SZ) );
-    for (int i=0; i<size; i+=64){
-        _mm_clflush( pg_al_loc + (offset%PG_SZ) + (i * 64) );
-    }
-    _mm_clflush( pg_al_loc + (offset%PG_SZ) + 64);
+	if ( use_flush == 1 ){
+		_mm_clflush( pg_al_loc + (offset%PG_SZ) );
+		for (int i=0; i<size; i+=64){
+			_mm_clflush( pg_al_loc + (offset%PG_SZ) + (i * 64) );
+		}
+		_mm_clflush( pg_al_loc + (offset%PG_SZ) + 64);
+	}
+
 	memcpy( (void *) rd_data, (void *) ( pg_al_loc + (offset % PG_SZ) ) , size );
+	if ( use_fence == 1 ){
+		_mm_mfence();
+	}
 	printf( "data hex:\n" );
 	int ctr=0;
 	while (ctr < size){
@@ -106,55 +77,16 @@ void read_dev_dram(uint64_t base, uint64_t offset, int size){
 		ctr += 1;
 	}	
 	printf("\n");
-	//
-	/*
-	printf( "%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x\n", 
-		*(uint8_t *)rd_data, *(uint8_t *)&rd_data[1], *(uint8_t *)&rd_data[2],*(uint8_t *)&rd_data[3],
-		*(uint8_t *)&rd_data[4], *(uint8_t *)&rd_data[5], *(uint8_t *)&rd_data[6],*(uint8_t *)&rd_data[7],
-		*(uint8_t *)&rd_data[8], *(uint8_t *)&rd_data[9], *(uint8_t *)&rd_data[10],*(uint8_t *)&rd_data[11],
-		*(uint8_t *)&rd_data[12], *(uint8_t *)&rd_data[13], *(uint8_t *)&rd_data[14],*(uint8_t *)&rd_data[15],
-		*(uint8_t *)&rd_data[16], *(uint8_t *)&rd_data[17], *(uint8_t *)&rd_data[18],*(uint8_t *)&rd_data[19],
-		*(uint8_t *)&rd_data[20], *(uint8_t *)&rd_data[21], *(uint8_t *)&rd_data[22],*(uint8_t *)&rd_data[23],
-		*(uint8_t *)&rd_data[24], *(uint8_t *)&rd_data[25], *(uint8_t *)&rd_data[26],*(uint8_t *)&rd_data[27],
-		*(uint8_t *)&rd_data[28], *(uint8_t *)&rd_data[29], *(uint8_t *)&rd_data[30],*(uint8_t *)&rd_data[31],
-		*(uint8_t *)&rd_data[32], *(uint8_t *)&rd_data[33], *(uint8_t *)&rd_data[34],*(uint8_t *)&rd_data[35],
-		*(uint8_t *)&rd_data[36], *(uint8_t *)&rd_data[37], *(uint8_t *)&rd_data[38],*(uint8_t *)&rd_data[39],
-		*(uint8_t *)&rd_data[40], *(uint8_t *)&rd_data[41], *(uint8_t *)&rd_data[42],*(uint8_t *)&rd_data[43],
-		*(uint8_t *)&rd_data[44], *(uint8_t *)&rd_data[45], *(uint8_t *)&rd_data[46],*(uint8_t *)&rd_data[47],
-		*(uint8_t *)&rd_data[48], *(uint8_t *)&rd_data[49], *(uint8_t *)&rd_data[50],*(uint8_t *)&rd_data[51],
-		*(uint8_t *)&rd_data[52], *(uint8_t *)&rd_data[53], *(uint8_t *)&rd_data[54],*(uint8_t *)&rd_data[55],
-		*(uint8_t *)&rd_data[56], *(uint8_t *)&rd_data[57], *(uint8_t *)&rd_data[58],*(uint8_t *)&rd_data[59],
-		*(uint8_t *)&rd_data[60], *(uint8_t *)&rd_data[61], *(uint8_t *)&rd_data[62],*(uint8_t *)&rd_data[63]);
-		*/
 	for (int i=0; i<w.ws_col; i++){
 		printf("-");
 	}
 	printf("\n");
 
 	printf( "data ascii:\n" );
-	/*
-	printf( "%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 
-		*(uint8_t *)rd_data, *(uint8_t *)&rd_data[1], *(uint8_t *)&rd_data[2],*(uint8_t *)&rd_data[3],
-		*(char *)&rd_data[4], *(char *)&rd_data[5], *(char *)&rd_data[6],*(char *)&rd_data[7],
-		*(char *)&rd_data[8], *(char *)&rd_data[9], *(char *)&rd_data[10],*(char *)&rd_data[11],
-		*(char *)&rd_data[12], *(char *)&rd_data[13], *(char *)&rd_data[14],*(char *)&rd_data[15],
-		*(char *)&rd_data[16], *(char *)&rd_data[17], *(char *)&rd_data[18],*(char *)&rd_data[19],
-		*(char *)&rd_data[20], *(char *)&rd_data[21], *(char *)&rd_data[22],*(char *)&rd_data[23],
-		*(char *)&rd_data[24], *(char *)&rd_data[25], *(char *)&rd_data[26],*(char *)&rd_data[27],
-		*(char *)&rd_data[28], *(char *)&rd_data[29], *(char *)&rd_data[30],*(char *)&rd_data[31],
-		*(char *)&rd_data[32], *(char *)&rd_data[33], *(char *)&rd_data[34],*(char *)&rd_data[35],
-		*(char *)&rd_data[36], *(char *)&rd_data[37], *(char *)&rd_data[38],*(char *)&rd_data[39],
-		*(char *)&rd_data[40], *(char *)&rd_data[41], *(char *)&rd_data[42],*(char *)&rd_data[43],
-		*(char *)&rd_data[44], *(char *)&rd_data[45], *(char *)&rd_data[46],*(char *)&rd_data[47],
-		*(char *)&rd_data[48], *(char *)&rd_data[49], *(char *)&rd_data[50],*(char *)&rd_data[51],
-		*(char *)&rd_data[52], *(char *)&rd_data[53], *(char *)&rd_data[54],*(char *)&rd_data[55],
-		*(char *)&rd_data[56], *(char *)&rd_data[57], *(char *)&rd_data[58],*(char *)&rd_data[59],
-		*(char *)&rd_data[60], *(char *)&rd_data[61], *(char *)&rd_data[62],*(char *)&rd_data[63]);
-		*/
 	ctr=0;
 	while (ctr < size){
 		printf( "%c" , *(char *)&rd_data[ctr] );
-		ctr += 2;
+		ctr++;
 	}	
 	printf("\n");
 	for (int i=0; i<w.ws_col; i++){
@@ -184,14 +116,33 @@ void write_dev_dram(uint64_t base, uint64_t offset, char * data, int size){
 	}
 	printf( "writing %s to memory address: 0x%016lx \n", data,  base + offset);
 
-	memcpy( (void *) ( pg_al_loc + (offset %PG_SZ) ), (void *) data, size );
-    /* flush cache lines of all data */
-	_mm_clflush( pg_al_loc + (offset%PG_SZ) );
-    for (int i=0; i<size; i+=64){
-        _mm_clflush( pg_al_loc + (offset%PG_SZ) + (i * 64) );
-    }
-    _mm_clflush( pg_al_loc + (offset%PG_SZ) + 64);
-    /* flush extra cache line in case data extends to next cache line */
+	if ( use_cache_fence == 1 ){
+		int rem=size;
+		int off=0;
+		while ( rem > 64 ){
+			memcpy( (void *) pg_al_loc + (offset %PG_SZ) + off, (void *) data + off, 64);
+			_mm_mfence();
+			rem-=64;
+			off+=64;
+		}
+		memcpy( (void *) pg_al_loc + (offset %PG_SZ) + off, (void *) data + off, rem);
+		_mm_mfence();
+	}
+	else{
+		memcpy( (void *) ( pg_al_loc + (offset %PG_SZ) ), (void *) data, size );
+	}
+	if ( use_fence == 1 ){
+		_mm_mfence();
+	}
+	if ( use_flush == 1 ){
+		/* flush cache lines of all data */
+		_mm_clflush( pg_al_loc + (offset%PG_SZ) );
+		for (int i=0; i<size; i+=64){
+			_mm_clflush( pg_al_loc + (offset%PG_SZ) + (i * 64) );
+		}
+		/* flush extra cache line in case data extends to next cache line */
+		_mm_clflush( pg_al_loc + (offset%PG_SZ) + 64);
+	}
 }
 int main(int argc, char ** argv)
 {
@@ -207,7 +158,7 @@ int main(int argc, char ** argv)
 
 
 	int opt;
-	while ((opt = getopt(argc, argv, "rwo:s:p:")) != -1) {
+	while ((opt = getopt(argc, argv, "rwfmco:s:p:")) != -1) {
 	   switch (opt) {
 	   case 'p':
             wr_data = (char *)malloc(sizeof(char) * strlen(optarg));
@@ -225,8 +176,17 @@ int main(int argc, char ** argv)
        case 'o':
            offset=strtoul(optarg, NULL, 10);
            break;
+       case 'f':
+           use_fence = 1;
+           break;
+       case 'c':
+           use_flush = 1;
+           break;
+       case 'm':
+           use_cache_fence = 1;
+           break;
 	   default: /* '?' */
-		   fprintf(stderr, "Usage: %s [-r] [-w] [-s size] [-p string] [-o offset] \n",
+		   fprintf(stderr, "Usage: %s [-r] [-f] [-c] [-m] [-w] [-s size] [-p string] [-o offset] \n",
 				   argv[0]);
 		   exit(EXIT_FAILURE);
 	   }
