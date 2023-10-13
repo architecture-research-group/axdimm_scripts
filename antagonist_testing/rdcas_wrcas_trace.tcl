973f525144d2b1b18e6ebdfdf07e8168eed68797
Neel Patel <n869p538@castor.ittc.ku.edu>
2023-10-13 07:44:30 -0500

    modified paths

--- antagonist_testing/rdcas_wrcas_trace.tcl
+++ antagonist_testing/rdcas_wrcas_trace.tcl
@@ -1,6 +1,6 @@
 
 # create_project -force -name comp_output
-open_project /home/a972m888/Project/Kansas_1.xpr
+open_project /home/n869p538/Project/Kansas_1.xpr
 open_hw_manager
 
 update_compile_order -fileset sources_1
@@ -9,9 +9,9 @@ connect_hw_server -allow_non_jtag
 
 open_hw_target
 
-set_property PROGRAM.FILE {/home/a972m888/Project/Kansas_1.runs/impl_1/axdimm_system_top.bit} [get_hw_devices xczu19_0]
-set_property PROBES.FILE {/home/a972m888/Project/Kansas_1.runs/impl_1/axdimm_system_top.ltx} [get_hw_devices xczu19_0]
-set_property FULL_PROBES.FILE {/home/a972m888/Project/Kansas_1.runs/impl_1/axdimm_system_top.ltx} [get_hw_devices xczu19_0]
+set_property PROGRAM.FILE {/home/n869p538/Project/Kansas_1.runs/impl_1/axdimm_system_top.bit} [get_hw_devices xczu19_0]
+set_property PROBES.FILE {/home/n869p538/Project/Kansas_1.runs/impl_1/axdimm_system_top.ltx} [get_hw_devices xczu19_0]
+set_property FULL_PROBES.FILE {/home/n869p538/Project/Kansas_1.runs/impl_1/axdimm_system_top.ltx} [get_hw_devices xczu19_0]
 current_hw_device [get_hw_devices xczu19_0]
 refresh_hw_device [lindex [get_hw_devices xczu19_0] 0]
 
@@ -24,5 +24,5 @@ for {set i 0} {$i < 1} {incr i} {
 	run_hw_ila hw_ila_1
 	wait_on_hw_ila hw_ila_1
 	upload_hw_ila_data hw_ila_1
-	write_hw_ila_data -force -csv_file /home/a972m888/HPCA_Rebuttal/ilaData/output.csv hw_ila_data_1
+	write_hw_ila_data -force -csv_file /home/n869p538/HPCA_Rebuttal/ilaData/output.csv hw_ila_data_1
 }
--- antagonist_testing/sp_util_run_and_plot.sh
+++ antagonist_testing/sp_util_run_and_plot.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 VIVADO_SETTINGS=/tools/xilinx/Vivado/2019.2/settings64.sh
-COPY_SCRIPT=./macrobench.sh
+REMOTE=n869p538@axdimm.ittc.ku.edu
+COPY_SCRIPT=/home/n869p538/axdimm_modules/antagonist_testing/macrobench.sh
+PLOT_SCRIPT=./Python_AxDIMM_Script/Arbiter_Script.py
 SIZE=$(( 2 ** 22 ))
 WAYMASK=0x0001
-OUTPUT_FILE=/home/a972m888/HPCA_Rebuttal/axdimm_scripts/antagonist_testing/${SIZE}_buffer_${WAYMASK}_Ways_.csv
+OUTPUT_FILE=$(pwd)/${SIZE}_buffer_${WAYMASK}_Ways_.csv
 
 source $VIVADO_SETTINGS
 TCL_SCRIPT=rdcas_wrcas_trace.tcl
 vivado -mode tcl -source $TCL_SCRIPT -log vivado.log &
-$COPY_SCRIPT $SIZE $WAYMASK
+ssh $REMOTE $COPY_SCRIPT $SIZE $WAYMASK 
+pid=$!
 wait
 
 mv /home/a972m888/HPCA_Rebuttal/ilaData/output.csv  $OUTPUT_FILE
 
+python3 $PLOT_SCRIPT -csv $OUTPUT_FILE
