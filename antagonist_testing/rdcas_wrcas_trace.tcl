6b2f17ccb2ae2fcbd92a4f3fe0b983155ed89565
Neel Patel <n869p538@castor.ittc.ku.edu>
2023-10-13 07:24:44 -0500

    run hw_ila and copy script

--- antagonist_testing/rdcas_wrcas_trace.tcl
+++ antagonist_testing/rdcas_wrcas_trace.tcl
@@ -1,5 +1,28 @@
-create_project -force -name comp_output
-run_hw_ila hw_ila_1
-wait_on_hw_ila hw_ila_1
-upload_hw_ila_data hw_ila_1
-write_hw_ila_data -csv_file /home/a972m888/HPCA_Rebuttal/ilaData/1workers-1-632-1way_$i.csv hw_ila_data_1a
+
+# create_project -force -name comp_output
+open_project /home/a972m888/Project/Kansas_1.xpr
+open_hw_manager
+
+update_compile_order -fileset sources_1
+open_hw_manager
+connect_hw_server -allow_non_jtag
+
+open_hw_target
+
+set_property PROGRAM.FILE {/home/a972m888/Project/Kansas_1.runs/impl_1/axdimm_system_top.bit} [get_hw_devices xczu19_0]
+set_property PROBES.FILE {/home/a972m888/Project/Kansas_1.runs/impl_1/axdimm_system_top.ltx} [get_hw_devices xczu19_0]
+set_property FULL_PROBES.FILE {/home/a972m888/Project/Kansas_1.runs/impl_1/axdimm_system_top.ltx} [get_hw_devices xczu19_0]
+current_hw_device [get_hw_devices xczu19_0]
+refresh_hw_device [lindex [get_hw_devices xczu19_0] 0]
+
+display_hw_ila_data [ get_hw_ila_data hw_ila_data_1 -of_objects [get_hw_ilas -of_objects [get_hw_devices xczu19_0] -filter {CELL_NAME=~"u_ila"}]]
+current_hw_device [get_hw_devices arm_dap_1]
+refresh_hw_device -update_hw_probes false [lindex [get_hw_devices arm_dap_1] 0]
+current_hw_device [get_hw_devices xczu19_0]
+
+for {set i 0} {$i < 1} {incr i} {
+	run_hw_ila hw_ila_1
+	wait_on_hw_ila hw_ila_1
+	upload_hw_ila_data hw_ila_1
+	write_hw_ila_data -force -csv_file /home/a972m888/HPCA_Rebuttal/ilaData/output.csv hw_ila_data_1
+}
--- antagonist_testing/sp_util_run_and_plot.sh
+++ antagonist_testing/sp_util_run_and_plot.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 VIVADO_SETTINGS=/tools/xilinx/Vivado/2019.2/settings64.sh
+COPY_SCRIPT=./macrobench.sh
+SIZE=$(( 2 ** 22 ))
+WAYMASK=0x0001
+OUTPUT_FILE=/home/a972m888/HPCA_Rebuttal/axdimm_scripts/antagonist_testing/${SIZE}_buffer_${WAYMASK}_Ways_.csv
 
 source $VIVADO_SETTINGS
 TCL_SCRIPT=rdcas_wrcas_trace.tcl
-vivado -mode tcl -source $TCL_SCRIPT -log vivado.log
+vivado -mode tcl -source $TCL_SCRIPT -log vivado.log &
+$COPY_SCRIPT $SIZE $WAYMASK
+wait
+
+mv /home/a972m888/HPCA_Rebuttal/ilaData/output.csv  $OUTPUT_FILE
+
