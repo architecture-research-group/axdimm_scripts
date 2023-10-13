77e1fa53f8b549f6d6ef691c210aef12a5ef3457
Neel Patel <n869p538@castor.ittc.ku.edu>
2023-10-13 07:31:52 -0500

    update macrobench to sleep after single copy

--- antagonist_testing/macrobench.sh
+++ antagonist_testing/macrobench.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 CORES=( "1" "2" "3" "4" "5" "6" "7" "8" )
 CORES=( "1" )
-SIZE=$(( 2 ** 20 ))
-SIZE=$(( 2 ** 20 )) + $(( 2 ** 19 )) + $(( 2 ** 16 )) + $(( 2 ** 15 ))
-WAYMASK=0x0001
+SIZE=${1}
+WAYMASK=${2}
 
 sudo pqos -R
 sudo pqos -e "llc:0=$WAYMASK"
@@ -14,14 +13,13 @@ rm -f microbench
 make microbench
 
 DST_BUFFERS_OFFSET=$(( SIZE * 9 ))
-DST_BUFFERS_OFFSET=$(( SIZE ))
 SRC_BUFFERS_OFFSET=$(( SIZE ))
 rm -f worker_log
-taskset -c 1 sudo ./microbench $(( SRC_BUFFERS_OFFSET + ( SIZE * i ) )) $(( DST_BUFFERS_OFFSET + (SIZE * i ) )) | tee -a worker_log &
-while [ "1" ]; do 
+while [ $iter -lt 3 ]; do 
 	for i in "${CORES[@]}"; do
-		#echo "taskset -c ${i} sudo ./worker $(( (i * SIZE) )) $(( ( (i * SIZE) ) + ( SIZE ) )) >> worker_log &"
-		taskset -c ${i} sudo ./microbench $(( SRC_BUFFERS_OFFSET + ( SIZE * i ) )) $(( DST_BUFFERS_OFFSET + (SIZE * i ) )) | tee -a worker_log &
+		echo "taskset -c ${i} sudo ./microbench $(( SRC_BUFFERS_OFFSET + ( SIZE * i ) )) $(( DST_BUFFERS_OFFSET + (SIZE * i ) )) | tee -a worker_log &"
+		iter=$(( iter + 1 ))
+		sleep 2
 	done
 	wait
 done
