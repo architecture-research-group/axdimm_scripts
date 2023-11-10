400229f0433eb7720aaa72b3f1ce0258478ff4ed
Neel Patel <n869p538@axdimm.ittc.ku.edu>
2023-11-10 14:58:24 -0600

    update address

--- src_dst_buffer_compcpy_allocations/microbench.c
+++ src_dst_buffer_compcpy_allocations/microbench.c
@@ -27,17 +27,16 @@
 
 #define SIZE 4096
 // Addresses matching in all fields aside from BA
-#define SRC_MATCH__1 0x1000E1A00
-#define DST_MATCH_1 0x1001E5900
+#define SRC_MATCH__1 0x100001C80
+#define DST_MATCH_1 0x100105A00
 
-
+#define PAGE_SIZE 0x000001000
 int main(int argc, char ** argv)
 {
 	int cdevfd=0;
 
 	uint64_t src_off=SRC_MATCH__1 - (SRC_MATCH__1%0x000001000); // Problem: matching addresses do not always fall on page boundaries -- need to map nearest page and copy between the specific addresses
 	uint64_t dst_off=DST_MATCH_1 - (DST_MATCH_1%0x000001000);
-
 
 	printf( "using src_off: 0x%lx\n", src_off );
 	printf( "using dst_off: 0x%lx\n", dst_off );
@@ -48,6 +47,7 @@ int main(int argc, char ** argv)
 		exit(-1);
 	}
 
+	
 	volatile char * src = (char *) mmap(NULL, SIZE, PROT_READ | PROT_WRITE, MAP_FILE | MAP_SHARED, cdevfd, src_off);
 	volatile char * dst = (char *) mmap(NULL, SIZE, PROT_READ | PROT_WRITE, MAP_FILE | MAP_SHARED, cdevfd, dst_off);
 	
@@ -62,9 +62,15 @@ int main(int argc, char ** argv)
 		exit(-1);
 	}
 	
-	src[0] = (uint64_t)src_off;
-	dst[0] = (uint64_t)dst_off;
-	_mm_clflush( (char *)src );
-	_mm_clflush( (char *)dst );
+	/* write/flush the matching addresses*/
+	int src_page_offset = (SRC_MATCH__1 % PAGE_SIZE);
+	int dst_page_offset = (DST_MATCH_1 % PAGE_SIZE);
+	src[src_page_offset] = (char)src_off;
+	dst[dst_page_offset] = (char)dst_off;
+	_mm_clflush( (char *)src+src_page_offset );
+	printf( "using src_off: 0x%lx\n", src+src_page_offset );
+	
+	_mm_clflush( (char *)dst+dst_page_offset );
+	printf( "using dst_off: 0x%lx\n", dst+dst_page_offset );
 
 }
