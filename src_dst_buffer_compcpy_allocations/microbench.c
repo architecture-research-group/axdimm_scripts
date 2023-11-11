b0ba7c09571718786ec201134084da70476e9b87
Neel Patel <n869p538@axdimm.ittc.ku.edu>
2023-11-10 21:59:09 -0600

    2 rdcas

--- src_dst_buffer_compcpy_allocations/microbench.c
+++ src_dst_buffer_compcpy_allocations/microbench.c
@@ -30,7 +30,16 @@
 #define SRC_MATCH__1 0x100001C80
 #define DST_MATCH_1 0x100105A00
 
+// Page offset addresses for validation
+#define SRC_MATCH__1 0x100002000
+#define DST_MATCH_1 0x100003000
+
+// 1MB, 2MB, 4MB, 8MB
+#define SRC_MATCH__1 0x109104313
+#define DST_MATCH_1 0x10A004313
+
 #define PAGE_SIZE 0x000001000
+#define CACHE_LINE_SIZE 0x000000040
 int main(int argc, char ** argv)
 {
 	int cdevfd=0;
@@ -38,8 +47,8 @@ int main(int argc, char ** argv)
 	uint64_t src_off=SRC_MATCH__1 - (SRC_MATCH__1%0x000001000); // Problem: matching addresses do not always fall on page boundaries -- need to map nearest page and copy between the specific addresses
 	uint64_t dst_off=DST_MATCH_1 - (DST_MATCH_1%0x000001000);
 
-	printf( "using src_off: 0x%lx\n", src_off );
-	printf( "using dst_off: 0x%lx\n", dst_off );
+	printf( "physical src page-aligned addr: 0x%lx\n", src_off );
+	printf( "physical dst page-aligned addr: 0x%lx\n", dst_off );
 
 	if ((cdevfd = open("/dev/mem", O_RDWR)) < 0)
 	{
@@ -65,12 +74,11 @@ int main(int argc, char ** argv)
 	/* write/flush the matching addresses*/
 	int src_page_offset = (SRC_MATCH__1 % PAGE_SIZE);
 	int dst_page_offset = (DST_MATCH_1 % PAGE_SIZE);
+	printf( "physical src byte addr: 0x%lx\n", src_off + (src_page_offset % CACHE_LINE_SIZE) );
+	// printf( "physical dst byte addr: 0x%lx\n", dst_off + (dst_page_offset % CACHE_LINE_SIZE) );
+
 	src[src_page_offset] = (char)src_off;
 	dst[dst_page_offset] = (char)dst_off;
-	_mm_clflush( (char *)src+src_page_offset );
-	printf( "using src_off: 0x%lx\n", src+src_page_offset );
-	
-	_mm_clflush( (char *)dst+dst_page_offset );
-	printf( "using dst_off: 0x%lx\n", dst+dst_page_offset );
+	// dst[dst_page_offset] = (char)dst_off;
 
 }
