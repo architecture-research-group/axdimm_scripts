316925c7a9f05c902e4971f9bb7897982e0f6595
Neel Patel <n869p538@pollux.ittc.ku.edu>
2023-11-10 18:21:56 -0600

    print row && col && full-string

--- src_dst_buffer_compcpy_allocations/address_decode.py
+++ src_dst_buffer_compcpy_allocations/address_decode.py
@@ -3,35 +3,45 @@ from helpers import *
 def wrcas(col_addrs, bg, ba, slot):
     # Convert col_addrs, bg, and ba to binary and append each bit to column_address, bank_group, and bank_address, respectively.
     col_addr = hex_to_bin(col_addrs)
-    bg_g   = hex_to_bin(bg)
-    ba_a   = hex_to_bin(ba)
-    bank_group = []
-    bank_address = []
-    column_address = []
-    for i in range(10):
-        column_address.append(col_addr[slot*2 + i*8])
-    for i in range(2):
-        bank_group.append(bg_g[slot*2 + i*8])
-        bank_address.append(ba_a[slot*2 + i*8])
-    print ("wrcas bg:{} ba:{} col:{}".format(''.join(bank_group), ''.join(bank_address), ''.join(column_address)))
-
-# This function performs a read operation.
-def rdcas(col_addrs, bg, ba, slot):
-    col_addr = hex_to_bin(col_addrs)
+    row_addr = hex_to_bin(col_addrs) # bad name -- sor
     bg_g   = hex_to_bin(bg)
     ba_a   = hex_to_bin(ba)
     bank_group = []
     bank_address = []
     column_address = []
     row_address = []
-    
+
+    for i in range(17):
+        row_address.append(row_addr[slot*2 + i*8]) 
     for i in range(10):
         column_address.append(col_addr[slot*2 + i*8])
     for i in range(2):
         bank_group.append(bg_g[slot*2 + i*8])
         bank_address.append(ba_a[slot*2 + i*8])
-    
-    print ("rdcas bg:{} ba:{} col:{}".format(''.join(bank_group), ''.join(bank_address), ''.join(column_address)))
+    print ("wrcas bg:{} ba:{} row:{} col:{}".format(''.join(bank_group), ''.join(bank_address), ''.join(row_address), ''.join(column_address)))
+
+# This function performs a read operation.
+def rdcas(col_addrs, bg, ba, slot):
+    # Convert col_addrs, bg, and ba to binary and append each bit to column_address, bank_group, and bank_address, respectively.
+    col_addr = hex_to_bin(col_addrs)
+    row_addr = hex_to_bin(col_addrs) # bad name -- sor
+    bg_g   = hex_to_bin(bg)
+    ba_a   = hex_to_bin(ba)
+    bank_group = []
+    bank_address = []
+    column_address = []
+    row_address = []
+
+    for i in range(17):
+        row_address.append(row_addr[slot*2 + i*8]) 
+    for i in range(10):
+        column_address.append(col_addr[slot*2 + i*8])
+    for i in range(2):
+        bank_group.append(bg_g[slot*2 + i*8])
+        bank_address.append(ba_a[slot*2 + i*8])
+    print ("rdcas bg:{} ba:{} col:{} row:{}".format(''.join(bank_group), ''.join(bank_address), ''.join(column_address), ''.join(row_address),))
+    print (''.join(bank_group) + ''.join(bank_address) + ''.join(column_address) + ''.join(row_address) )
+
 
 # This function decodes a given command.
 def command_decoder(given_row):
